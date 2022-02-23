83bce6450363c1d37d6522d8dd6e216e79f81a71
webdeveloper-pk <mabdullah.rasheed8@gmail.com>
2022-02-24 03:00:48 +0500

    fixing issue

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import store from "../src/redux/store/store";
-import "antd/dist/antd.css";
+import "antd/dist/antd.min.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "./index.css";
